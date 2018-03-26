1ab5900c881e3e7432f810bddafdfd41655dad1b
Faiyaz Ansari <37465786+ansarifaiyaz28@users.noreply.github.com>
2018-03-26 21:59:20 +0800

    Add files via upload
    this function will calculate fast exponential. This is a solution to programming problem 2.1 from book- "Cryptography and Network Security", seventh edition by- William Stallings

---
+++ fastExponential.py
@@ -0,0 +1,21 @@
+
+
+def f(x,e,m):
+    X = x
+    E = e
+    Y = 1
+    while E > 0:
+        if E % 2 == 0:
+            X = (X * X) % m
+            E = E/2
+        else:
+            Y = (X * Y) % m
+            E = E - 1
+    return Y
+
+
+
+
+
+
+
